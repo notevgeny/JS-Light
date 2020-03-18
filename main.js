6962224ce2ad05a98fe4a47fab3d7af3e166c95e
Евгений Климович <33512176+notevgeny@users.noreply.github.com>
2020-03-19 02:05:50 +0300

    all works

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 'use strict';
 
-const statusMessage = document.createElement('div');
-statusMessage.style.fontSize = '2rem;';
-const question = document.querySelector('.form-question');
+const statusMessage = document.createElement('div'),
+      inputMessage = 'Заполните все поля!',
+      question = document.querySelector('.form-question'),
+      questionBtn = document.querySelector('.consultation-btn');
+      statusMessage.style.fontSize = '2rem;';
 
 // является ли модальное окно калькулятором/акцией
 let isCalc = '';
@@ -14,6 +16,7 @@ const removeMessage = () => {
   }, 3000);
 };
 
+// стирание данных инпутов
 const deleteInputs = (event) => {
   let target = event.target;
   if (target.querySelector('input[name = "user_phone"]')){
@@ -46,13 +49,10 @@ let calcMemory = {
 };
 
   
-
 // модальные окна
 const togglePopup = (popupModal, selectorButton, statusCalc) => {
   
- const popup = document.querySelector(popupModal),
- questionBtn = document.querySelector('.consultation-btn');
- const inputMessage = 'Заполните все поля!';
+ const popup = document.querySelector(popupModal);
  let popupButton = document.querySelectorAll(selectorButton);
 
 popupButton.forEach((item) => {
@@ -113,28 +113,31 @@ const askQuestion = () => {
 askQuestion();
 
 
+
 // показать больше
+
 const showMore = () => {
-  const addSentenceBtn = document.querySelector('.add-sentence-btn'),
-        sentence = document.querySelector('.sentence'),
-        itemBlocks = sentence.querySelectorAll('.col-xs-12');
-  addSentenceBtn.addEventListener('click', (event) => {
-   itemBlocks.forEach((elem) => {
-    elem.classList.remove('visible-sm-block');
-    elem.classList.remove('hidden');
-   });
-   addSentenceBtn.style.display = 'none';
-  })
- }
+ const addSentenceBtn = document.querySelector('.add-sentence-btn'),
+       sentence = document.querySelector('.sentence'),
+       itemBlocks = sentence.querySelectorAll('.col-xs-12');
+ addSentenceBtn.addEventListener('click', (event) => {
+  itemBlocks.forEach((elem) => {
+   elem.classList.remove('visible-sm-block');
+   elem.classList.remove('hidden');
+  });
+  addSentenceBtn.style.display = 'none';
+ })
+}
 showMore();
 
 
 // часто задаваемые вопросы - аккордеон
 
-const accordion = () => {
+const showAccordion = () => {
  const accordionTwo = document.getElementById('accordion-two'),
-       panel = accordionTwo.querySelectorAll('.panel'),
        panelCollapse = accordionTwo.querySelectorAll('.panel-collapse');
+
+let panel = accordionTwo.querySelectorAll('.panel');
 
  for (let i = 0; i < panel.length; i++) {
   panel[i].addEventListener('click', function(event) {
@@ -149,69 +152,68 @@ const accordion = () => {
   })
  }
 }
-accordion();
+showAccordion();
 
 
 // онлайн-конструктор септика
 
 const constructor = () => {
-// общий блок всего калькулятора
-const accordion = document.getElementById('accordion'),
-
-// каждый этап в калькуляторе
-panel = accordion.querySelectorAll('.panel'),
-// содержимое в окне этапа в калькуляторе
-panelBody = accordion.querySelectorAll('.panel-body'),
-// окно этапа в калькуляторе
-panelCollapse = accordion.querySelectorAll('.panel-collapse'),
-// второй этап в калькуляторе
-panelTwo = document.getElementById('collapseTwo'),
-
-panelHeading = accordion.querySelectorAll('.panel-heading'),
-
-// кнопка "Следующий шаг"
-panelBodyBtn = accordion.querySelectorAll('.construct-btn'),
-
-// ПЕРВЫЙ КОЛОДЕЦ (ПРИЕМНЫЙ) 
-  // диаметр первого колодца
+  // общий блок всего калькулятора
+ const accordion = document.getElementById('accordion'),
+  
+  // окно этапа в калькуляторе
+  panelCollapse = accordion.querySelectorAll('.panel-collapse'),
+  // второй этап в калькуляторе
+  panelTwo = document.getElementById('collapseTwo'),
+  
+  panelHeading = accordion.querySelectorAll('.panel-heading'),
+  
+  // кнопка "Следующий шаг"
+  panelBodyBtn = accordion.querySelectorAll('.construct-btn'),
+  
+  // ПЕРВЫЙ КОЛОДЕЦ (ПРИЕМНЫЙ) 
+    // диаметр первого колодца
   wellDiameterOne = document.getElementById('one-well-diameter'),
-  // количество колец первого
+    // количество колец первого
   wellRingsOne = document.getElementById('one-well-rings'),
-
-// ВТОРОЙ КОЛОДЕЦ (ПРИЕМНЫЙ)
-// текстовое поле
-wellTextTwo = document.getElementById('two-well'),
-// диаметр второго
-wellDiameterTwo = document.getElementById('two-well-diameter'),
-// колво колеу второго
-wellRingsTwo = document.getElementById('two-well-rings'),
-
-
-// селекты первого и второго колодцев
-// первого
-wellDiameterOneSelect = wellDiameterOne.querySelector('.expand'),
-wellRingsOneSelect = wellRingsOne.querySelector('.expand'),
-// второго
-wellRingsTwoSelect = wellRingsTwo.querySelector('.expand'),
-wellDiameterTwoSelect = wellDiameterTwo.querySelector('.expand'),
-
-// окно результата
-calcResult = document.getElementById('calc-result'),
-// переключатели в первом и третьем этапе калькулятора
-inner = document.querySelectorAll('.onoffswitch-inner');
-
-// расстояние до дома
-const distance = document.getElementById('distance');
-
-
+  
+  // ВТОРОЙ КОЛОДЕЦ (ПРИЕМНЫЙ)
+  // текстовое поле
+  wellTextTwo = document.getElementById('two-well'),
+  // диаметр второго
+  wellDiameterTwo = document.getElementById('two-well-diameter'),
+  // колво колеу второго
+  wellRingsTwo = document.getElementById('two-well-rings'),
+  
+  
+  // селекты первого и второго колодцев
+  // первого
+  wellDiameterOneSelect = wellDiameterOne.querySelector('.expand'),
+  wellRingsOneSelect = wellRingsOne.querySelector('.expand'),
+  // второго
+  wellRingsTwoSelect = wellRingsTwo.querySelector('.expand'),
+  wellDiameterTwoSelect = wellDiameterTwo.querySelector('.expand'),
+  
+  // окно результата
+  calcResult = document.getElementById('calc-result'),
+  // переключатели в первом и третьем этапе калькулятора
+  
+  inner = document.querySelectorAll('.onoffswitch-checkbox');
+  
+  // расстояние до дома
+  const distance = document.getElementById('distance');
+  
+  
   let checkbox = document.querySelectorAll('.onoffswitch-checkbox');
-
+  
+  
+  
   // хождение по этапам калькулятора
   function toggleAcc (index, next){
     if (next){
       if (next < panelHeading.length){
-      panelCollapse[index].classList.remove('in');
-      panelCollapse[next].classList.add('in');
+        panelCollapse[index].classList.remove('in');
+        panelCollapse[next].classList.add('in');
       }
     }
     else {
@@ -230,6 +232,7 @@ const distance = document.getElementById('distance');
   for (let i = 0; i < panelHeading.length; i++) {
     panelHeading[i].addEventListener('click', (event) => {
       event.preventDefault();
+  
       toggleAcc(i);
     });
     panelBodyBtn[i].addEventListener('click', (event) => {
@@ -237,222 +240,163 @@ const distance = document.getElementById('distance');
       toggleAcc(i, i+1);
     });
   }
-
-  // количество колодцев
-  const showWells = (cameras) => {
-    if (cameras === 1) {
-      wellTextTwo.style.display = 'none';
-      wellDiameterTwo.style.display = 'none';
-      wellRingsTwo.style.display = 'none';
-      return;
-    }
-    else {
-      wellTextTwo.style.display = '';
-      wellDiameterTwo.style.display = '';
-      wellRingsTwo.style.display = '';
-      return;
-    }
-  }
-
-  // мат логика калькулятора
-  const sumDiameter = (cameras, floors) => {
-
-    let diameterFactorOne = 0, diameterFactorTwo = 0,
-          ringsFactorOne = 0, ringsFactorTwo = 0;
-    const diameterOneValue = +wellDiameterOneSelect.value;
-    const ringsOneValue = +wellRingsOneSelect.value;
-    let price = 0;
-
-    let sumDiameter = 0;
-    let sumRings = 0;
-    let floorsValue = 0;
-
-    diameterFactorOne = diameterOneValue === 1.4 ? 0 : 0.2;
-
-    if (ringsOneValue === 1) {
-      ringsFactorOne = 0;
-    }
-
-    else if (ringsOneValue === 2){
-      ringsFactorOne = 0.3;
-    }
-    else ringsFactorOne = 0.5;
-
-
-    if (cameras === 2) {
-      price = 15000;
-
-      const diameterTwoValue = +wellDiameterTwoSelect.value;
-      const ringsTwoValue = +wellRingsTwoSelect.value;
-          
-      // диаметр2
-      diameterFactorTwo = diameterTwoValue === 1.4 ? 0 : 0.2;
-
-      // кольца2
-      if (ringsTwoValue === 1) {
-        ringsFactorTwo = 0;
-      }
-      else if (ringsTwoValue === 2){
-        ringsFactorTwo = 0.3;
-      }
-      else ringsFactorTwo = 0.5;
-
-      sumDiameter = (price * diameterFactorOne) + (price * diameterFactorTwo);
-      sumRings = (price * ringsFactorOne) + (price * ringsFactorTwo);
-      
-
-      if (floors === 1) {
-        floorsValue = 2000;
-      }
-
-      calcMemory.diameterOneValue = diameterOneValue;
-      calcMemory.ringsOneValue = ringsOneValue;
-      calcMemory.diameterTwoValue = diameterTwoValue;
-      calcMemory.ringsTwoValue = ringsTwoValue;
-    }
-    else if (cameras === 1){
-      price = 10000;
-
-      sumDiameter = price * diameterFactorOne;
-      sumRings = price * ringsFactorOne;
-
-          if (floors === 1) {
-            floorsValue = 1000;
-          }
-
-      calcMemory.diameterOneValue = diameterOneValue; 
-      calcMemory.ringsOneValue = ringsOneValue;
-      calcMemory.diameterTwoValue = null;
-      calcMemory.ringsTwoValue = null;
-    }
-
-    calcMemory.sum = price + sumDiameter + sumRings + floorsValue;
-    calcResult.value = `${calcMemory.sum} рублей`;
-  }
-
   
-  const getDiameter = (cameras, floors) => {
-    sumDiameter(calcMemory.cameras, calcMemory.floors);
+    // количество колодцев
+    const showWells = (cameras) => {
+      if (cameras === 1) {
+        wellTextTwo.style.display = 'none';
+        wellDiameterTwo.style.display = 'none';
+        wellRingsTwo.style.display = 'none';
+        return;
+      }
+      else {
+        wellTextTwo.style.display = '';
+        wellDiameterTwo.style.display = '';
+        wellRingsTwo.style.display = '';
+        return;
+      }
+    }
+  
+    // мат логика калькулятора
+    const sumDiameter = (cameras, floors) => {
+  
+      let diameterFactorOne = 0, diameterFactorTwo = 0,
+            ringsFactorOne = 0, ringsFactorTwo = 0;
+      const diameterOneValue = +wellDiameterOneSelect.value;
+      const ringsOneValue = +wellRingsOneSelect.value;
+      let price = 0;
+  
+      let sumDiameter = 0;
+      let sumRings = 0;
+      let floorsValue = 0;
+  
+      diameterFactorOne = diameterOneValue === 1.4 ? 0 : 0.2;
+  
+      if (ringsOneValue === 1) {
+        ringsFactorOne = 0;
+      }
+  
+      else if (ringsOneValue === 2){
+        ringsFactorOne = 0.3;
+      }
+      else ringsFactorOne = 0.5;
+  
+  
+      if (cameras === 2) {
+        price = 15000;
+  
+        const diameterTwoValue = +wellDiameterTwoSelect.value;
+        const ringsTwoValue = +wellRingsTwoSelect.value;
+            
+        // диаметр2
+        diameterFactorTwo = diameterTwoValue === 1.4 ? 0 : 0.2;
+  
+        // кольца2
+        if (ringsTwoValue === 1) {
+          ringsFactorTwo = 0;
+        }
+        else if (ringsTwoValue === 2){
+          ringsFactorTwo = 0.3;
+        }
+        else ringsFactorTwo = 0.5;
+  
+        sumDiameter = (price * diameterFactorOne) + (price * diameterFactorTwo);
+        sumRings = (price * ringsFactorOne) + (price * ringsFactorTwo);
+        
+  
+        if (floors === 1) {
+          floorsValue = 2000;
+        }
+  
+        calcMemory.diameterOneValue = diameterOneValue;
+        calcMemory.ringsOneValue = ringsOneValue;
+        calcMemory.diameterTwoValue = diameterTwoValue;
+        calcMemory.ringsTwoValue = ringsTwoValue;
+      }
+      else if (cameras === 1){
+        price = 10000;
+  
+        sumDiameter = price * diameterFactorOne;
+        sumRings = price * ringsFactorOne;
+  
+            if (floors === 1) {
+              floorsValue = 1000;
+            }
+  
+        calcMemory.diameterOneValue = diameterOneValue; 
+        calcMemory.ringsOneValue = ringsOneValue;
+        calcMemory.diameterTwoValue = null;
+        calcMemory.ringsTwoValue = null;
+      }
+  
+      calcMemory.sum = price + sumDiameter + sumRings + floorsValue;
+      calcResult.value = `${calcMemory.sum} рублей`;
+    }
+  
+  
+    panelTwo.addEventListener('change', event => {
+        
+      const target = event.target;
+        if (target.matches('select')) {
+          sumDiameter(calcMemory.cameras, calcMemory.floors);
+      }
+    });
+  
+  // сколько камер содержит колодец
+  const getSepticType = () => {
+    // if (checkbox[0].checked === true){
+    //   calcMemory.cameras = 1;
+    // }
+    // else {
+      calcMemory.cameras = 1;
+    // }
   }
-
-  panelTwo.addEventListener('change', event => {
+  // первичное значение днища
+  const getFloorsType = () => {
+    // if (checkbox[1].checked === true){
+    //   calcMemory.floors = 1;
+    // }
+    // else {
+      calcMemory.floors = 0;
+    // }
+  }
+  
+  // сколько метров до дома
+  const getDistance = () => {
+    distance.addEventListener('input', event => {
+      distance.value = distance.value.replace(/[^\d\+]/g, '');
+      calcMemory.distance = distance.value;
+    })
+  }
+  // определение необходимого чекбокса
+  inner.forEach((item) =>{
+    item.addEventListener('change', (event) =>{
+      if (event.target.closest('.first-checkbox')){
+  
+        if (event.target.checked) {
+          calcMemory.cameras = 1;
+        }
+        else  calcMemory.cameras = 2;
+        showWells(calcMemory.cameras);
+      }
+      if (event.target.closest('.second-checkbox')){
+        if (event.target.checked) {
+          calcMemory.floors = 1;
+        }
+        else  calcMemory.floors = 0;
+        showWells(calcMemory.cameras);
+      }
+      sumDiameter(calcMemory.cameras, calcMemory.floors);
       
-    const target = event.target;
-      if (target.matches('select')) {
-        sumDiameter(calcMemory.cameras, calcMemory.floors);
-    }
-  });
-
-// сколько камер содержит колодец
-const getSepticType = () => {
-  if (checkbox[0].checked === true){
-    calcMemory.cameras = 1;
-    return calcMemory.cameras;
-  }
-  else {
-    calcMemory.cameras = 2;
-    return calcMemory.cameras;
-  }
-}
-// первичное значение днища
-const getFloorsType = () => {
-  if (checkbox[1].checked === true){
-    calcMemory.floors = 1;
-    return calcMemory.floors;
-  }
-  else {
-    calcMemory.floors = 0;
-    return calcMemory.floors;
-  }
-}
-
-// есть ли днище у колодца
-const getFloorValue = (floorStatus) => {
-  if (floorStatus === 0) {
-    calcMemory.floors = 0;
-    return calcMemory.floor;
-  }
-  else {
-    calcMemory.floors = 1;
-    return calcMemory.floor;
-  }
-}
-
-// первичное количество камер у колодца
-const getCamerasValue = (camerasStatus) => {
-  if (camerasStatus === 1) {
-    calcMemory.cameras = 1;
-    return calcMemory.cameras;
-  }
-  else {
-    calcMemory.cameras = 2;
-    return calcMemory.cameras;
-  }
-}
-
-// переключатель чекбоксов
-const checkTheStage = (checkboxValue) => {
-  if (checkboxValue === 1) {
-  if (checkbox.checked === true){
-    checkbox.checked = false;
-    getCamerasValue(2);
-    
-  }
-  else {
-    checkbox.checked = true;
-    getCamerasValue(1);
-    
-  }
-}
-else {
-  if (checkbox.checked === true){
-    checkbox.checked = false;
-    getFloorValue(0);
-    
-  }
-  else {
-    checkbox.checked = true;
-    getFloorValue(1);
-    
-  }
-}
-}
-
-// сколько метров до дома
-const getDistance = () => {
-  distance.addEventListener('input', event => {
-    distance.value = distance.value.replace(/[^\d\+]/g, '');
-    calcMemory.distance = distance.value;
+    })
   })
-}
-// определение необходимого чекбокса
-inner.forEach((item) =>{
-  item.addEventListener('click', (event) =>{
-    if (event.target.parentNode.htmlFor === 'myonoffswitch'){
-      checkbox = document.querySelectorAll('.onoffswitch-checkbox');
-      checkbox = checkbox[0];
-      checkTheStage(1);
-      showWells(calcMemory.cameras);
-      getDiameter(calcMemory.cameras, calcMemory.floors);
-    }
-    else if (event.target.parentNode.htmlFor === 'myonoffswitch-two'){
-      checkbox = document.querySelectorAll('.onoffswitch-checkbox');
-      checkbox = checkbox[1];
-      checkTheStage(2);
-      showWells(calcMemory.cameras);
-      getDiameter(calcMemory.cameras, calcMemory.floors);
-    }
-    
-  })
-})
-
- getSepticType();
- getFloorsType();
- showWells(calcMemory.cameras);
- getDiameter(calcMemory.cameras, calcMemory.floors);
- getDistance();
- 
-}
+  
+   getSepticType();
+   getFloorsType();
+   showWells(calcMemory.cameras);
+   getDistance();
+   
+  }
  constructor();
  
 
@@ -471,7 +415,7 @@ inner.forEach((item) =>{
     }
  
     const checkInputPhone = (target) => {
-      target.value = target.value.replace(/[^\d\+]/g, '');
+       target.value = target.value.replace(/[^\d\+]/g, '');
     }
  
     let target = event.target;
